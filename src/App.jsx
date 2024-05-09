88be2db89067d12d8ebcebf415fdd691f5401aea
Emiliano Daza <emiliano.daza@santexgroup.com>
2024-05-09 14:42:28 -0300

    Finished solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import FoodGrid from "./components/FoodGrid";
+import Header from "./components/Header";
+import { CartContextProvider } from "./store/CartContext";
+
 function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <CartContextProvider>
+        <Header />
+        <FoodGrid />
+      </CartContextProvider>
     </>
   );
 }
